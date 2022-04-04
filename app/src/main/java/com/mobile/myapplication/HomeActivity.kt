48dbe5fb0a166c88f37686093238a8f8bff16808
dipaw626 <dipawirantanu@gmail.com>
2022-04-04 16:36:43 +0700

    Initial commit

---
+++ app/src/main/java/com/mobile/myapplication/HomeActivity.kt
@@ -0,0 +1,51 @@
+package com.mobile.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.mobile.myapplication.home.HomeFragment
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var btnHome: Button
+    private lateinit var btnProfile: Button
+    private lateinit var btnSetting: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        btnHome = findViewById(R.id.btnHome)
+        btnProfile = findViewById(R.id.btnProfile)
+        btnSetting = findViewById(R.id.btnSetting)
+
+
+//        btnProfile.setOnClickListener{
+//            val intent = Intent(this, ProfileActivity::class.java)
+//            startActivity(intent)
+//        }
+//
+//        btnSetting.setOnClickListener{
+//            val intent = Intent(this, ProfileActivity::class.java)
+//            startActivity(intent)
+//        }
+//
+//        btnHome.setOnClickListener{
+//            val intent = Intent( this, HomeActivity::class.java)
+//            intent.putExtra("username","Dipa")
+//            startActivity(intent)
+//        }
+
+        btnHome.setOnClickListener{
+            loadFragment(HomeFragment())
+        }
+
+    }
+
+    private  fun  loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, fragment)
+            .commitNow()
+    }
+}
